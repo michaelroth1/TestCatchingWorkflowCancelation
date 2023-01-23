0ad11572330194e87817f9bd62a06373ba1fb252
mitoSoft <michael.mr.roth@gmail.com>
2023-01-24 00:28:05 +0100

    initial

---
+++ CatchCancelEventStateMachine.cs
@@ -0,0 +1,50 @@
+﻿using mitoSoft.Workflows;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using mitoSoft.Workflows.EventArgs;
+
+namespace TestCatchingWorkflowCancelation
+{
+    internal class CatchCancelEventStateMachine : StateMachine
+    {
+        public Invoker Invoker { get; set; } = null!;
+
+        public List<string> Logger { get; } = new List<string>();
+
+        public CatchCancelEventStateMachine()
+        {
+            this
+              .AddNode(new State("Start", () => Debug.WriteLine("Start")))
+              .AddNode(new State("State1", () => Debug.WriteLine("State1")))
+              .AddNode(new State("State2", () =>
+              {
+                  Thread.Sleep(1000);
+                  Debug.WriteLine("State2");
+              }))
+              .AddNode(new State("End", () => Debug.WriteLine("End")))
+              .AddEdge("Start", "State1", () => { return true; })
+              .AddEdge("State1", "State2", () => { return true; })
+              .AddEdge("State2", "End", () => { return true; });
+        }
+
+        public override void Invoke(CancellationToken cancellationToken, DateTime timeout)
+        {
+            if (this.Invoker != null)
+            {
+                this.Invoker.Faulted += Invoker_Faulted!;
+            }
+
+            base.Invoke(cancellationToken, timeout);
+        }
+
+        private void Invoker_Faulted(object sender, StateMachineFaultedEventArgs e)
+        {
+            //Do something here
+            //...
+
+            this.Logger.Add("CancelCatched");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using mitoSoft.Workflows;
+using TestCatchingWorkflowCancelation;
+
+Console.WriteLine("Start");
+
+var stateMachine = new CatchCancelEventStateMachine();
+
+var invoker = new Invoker(stateMachine);
+invoker.Completed += (sender, args) =>
+{
+    stateMachine.Logger.Add("Ended");
+};
+invoker.Faulted += (sender, args) =>
+{
+    stateMachine.Logger.Add("Canceled");
+};
+
+stateMachine.Invoker = invoker;
+
+var t = invoker.Invoke();
+
+stateMachine.Logger.Add("Started");
+
+Thread.Sleep(100);
+
+stateMachine.Logger.Add("BeforeCancelRequested");
+invoker.Cancel();
+
+t.Wait();
+
+Console.WriteLine(string.Join("->", stateMachine.Logger));
+
+Console.ReadLine();
